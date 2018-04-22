f03776f3d17987a75f80ac840e18972a1d59a0cc
Alex Bu <bkpfx000@gmail.com>
2018-04-22 16:57:57 +0800

    add referer to download page; return all baidu disk url for one app

--- getapppage.py
+++ getapppage.py
@@ -2,24 +2,29 @@
 # encoding: utf-8
 import requests
 from bs4 import BeautifulSoup
+from getapplist import xclient_site
 
-def get_download_page(url):
+def get_download_page(url, referer):
     # get this page will redirect to app intro page
     # guess: set referer field
-    r = requests.get(url)
+    r = requests.get(url, headers={'referer': referer})
     soup = BeautifulSoup(r.content, "html.parser")
-    print(soup.prettify())
+    link = soup.find('a', class_='btn_down_link pop_btn')
+    disk_url = link.get('data-link')
+    print(disk_url)
+    disk_pass = link.get('data-clipboard-text')
+    print(disk_pass)
 
 def get_app_page(url):
+    id = xclient_site().get_id()
+    url += id
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
     links = soup.findAll('a', class_='btn-download')
     download_url_list = [link.get('href') for link in links if u'百度云盘' in link.text]
 
-    print(download_url_list)
-
-    get_download_page(download_url_list[0])
+    for one_url in download_url_list:
+        get_download_page(one_url, url)
 
 if __name__ == '__main__':
-    page = get_app_page('http://xclient.info/s/typinator.html')
-    print(page)
+    get_app_page('http://xclient.info/s/typinator.html')
